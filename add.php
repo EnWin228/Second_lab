983cf8ca6e7e76daf8bb4616a3d8fb7c06b25765
EnWin228 <121401231+EnWin228@users.noreply.github.com>
2022-12-25 23:26:32 +0300

    Update add.php

--- add.php
+++ add.php
@@ -2,23 +2,14 @@
 
 $result = $_POST['list'];
 
-
-$uploaddir = './img/';
-$uploadfile = $uploaddir . basename($_FILES['file']['name']);
-
-move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
-
-if(!move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)){
-    echo "Ошибка загрузки файла";
-}
-if (($result =='') or ($_FILES['file']['name']=='')) {
+if ($result =='') {
     header('Location: index.php');
 }
 require 'db.php';
 date_default_timezone_set('Europe/Moscow');
 $time = date("Y-d-m H:i:s");
 
-$resultall = mysqli_query($db, "INSERT INTO posts (text,likes,dtime,image) VALUES('$result','0','$time','$uploadfile')");
+$resultall = mysqli_query($db, "INSERT INTO posts (text,likes,dtime) VALUES('$result','0','$time')");
 
 header('Location: index.php');
 
